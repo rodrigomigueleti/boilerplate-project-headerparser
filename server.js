1b235af16ccbcb3147a1aa7aa04368f19a2b90de
rodrigomigueleti <rodrigo.alvesmigueleti@gmail.com>
2021-11-11 16:20:47 -0300

    concluded

--- server.js
+++ server.js
@@ -25,9 +25,39 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-
+app.get("/api/whoami", function (req, res) {
+  res.json({
+  	ipaddress : req.ip,
+  	language: req.headers["accept-language"], 
+  	software: req.headers["user-agent"]
+  });
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+/*
+http-headers
+*{"ipaddress": {
+	"host":"127.0.0.1:51210",
+	"connection":"keep-alive",
+	"sec-ch-ua":"\"Google Chrome\";v=\"95\",
+	\"Chromium\";v=\"95\",
+	\";Not A Brand\";v=\"99\"",
+	"sec-ch-ua-mobile":"?0",
+	"sec-ch-ua-platform":"\"Windows\"",
+	"upgrade-insecure-requests":"1",
+	"user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36",
+	"accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,/;q=0.8,application/signed-exchange;v=b3;q=0.9",
+	"sec-fetch-site":"none",
+	"sec-fetch-mode":"navigate",
+	"sec-fetch-user":"?1",
+	"sec-fetch-dest":"document",
+	"accept-encoding":"gzip, deflate, br",
+	"accept-language":"pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7
+	"}
+  }
+
+*/
