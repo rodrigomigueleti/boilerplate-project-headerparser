58d8bd64a69db0f7535605c807a43f527bdacf12
rodrigomigueleti <rodrigo.alvesmigueleti@gmail.com>
2021-11-11 16:32:06 -0300

    Update server.js

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.get("/api/hello", function (req, res) {
 
 app.get("/api/whoami", function (req, res) {
   res.json({
-  	ipaddress : req.headers["host"],
+  	ipaddress : req.headers["x-forwarded-for"],
   	language: req.headers["accept-language"], 
   	software: req.headers["user-agent"]
   });
